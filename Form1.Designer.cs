3a63c5cd264d6726057754329ea18374f33ce9b4
ganggbang <ganggbang@mail.ru>
2015-11-24 18:15:40 +0300

    11

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace EndClothing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bn_start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bn_start
+            // 
+            this.bn_start.Location = new System.Drawing.Point(445, 118);
+            this.bn_start.Name = "bn_start";
+            this.bn_start.Size = new System.Drawing.Size(422, 205);
+            this.bn_start.TabIndex = 0;
+            this.bn_start.Text = "START";
+            this.bn_start.UseVisualStyleBackColor = true;
+            this.bn_start.Click += new System.EventHandler(this.bn_start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 477);
+            this.Controls.Add(this.bn_start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bn_start;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EndClothing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        string GetFirstByRegex(string pattern, string text)
+        {
+            var rgx = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            var matches = rgx.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                return match.Value.ToString();
+            }
+            return string.Empty;
+        }
+
+
+        List<string> GetListByRegex(string pattern, string text)
+        {
+            List<string> items = new List<string>();
+
+            var rgx = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            var matches = rgx.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                items.Add(match.Value.ToString());   
+            }
+            return items;
+        }
+
+        string _getRequest(string url, string refer)
+        {
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                httpWebRequest.AllowAutoRedirect = true;
+                httpWebRequest.Method = "GET";
+
+                httpWebRequest.Referer = refer;
+
+                using (var httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse())
+                {
+
+                    using (var stream = httpWebResponse.GetResponseStream())
+                    {
+
+                        using (var reader = new StreamReader(stream, Encoding.GetEncoding(httpWebResponse.CharacterSet)))
+                        {
+                            return reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                using (var sr = new StreamReader(ex.Response.GetResponseStream()))
+                    return sr.ReadToEnd();
+            }
+        }
+
+        
+
+        string parseIntList2Str(List<string> list_expression)
+        {
+
+            int v_exp = 0;
+            string parse_to_int = "";
+            foreach (String exp in list_expression)
+            {
+                if (exp.Length > 1)
+                {
+                    v_exp = 0;
+                    v_exp += GetListByRegex(@"\!\!\[\]", exp).Count();
+                    v_exp += GetListByRegex(@"\!{1}\+", exp).Count();
+                    parse_to_int += v_exp.ToString();
+                }
+            }
+
+            return parse_to_int;
+        }
+
+        string Minus(string v1,string v2)
+        {
+
+            var i = Int32.Parse(v1) - Int32.Parse(v2);
+            return i.ToString();
+        }
+
+        string Plus(string v1, string v2)
+        {
+
+            var i = Int32.Parse(v1) + Int32.Parse(v2);
+            return i.ToString();
+        }
+
+
+        string Multiply(string v1, string v2)
+        {
+
+            var i = Int32.Parse(v1) * Int32.Parse(v2);
+            return i.ToString();
+        }
+
+        private void bn_start_Click(object sender, EventArgs e)
+        {
+            var url = "http://endclothing.com/";
+            var html = _getRequest(url, "");
+
+            //var t,r,a,f, WOlLQxg={"xcWIr":
+
+            var t = GetFirstByRegex(@"[0-9\.]+", url).Length;
+
+            string var = GetFirstByRegex(@"parseInt\([a-zA-Z0-9.]+", html).Substring(9);
+
+            string var_without_obj = GetFirstByRegex(@"parseInt\([a-zA-Z0-9]+", html).Substring(9);
+
+            string obj = var.Substring(var_without_obj.Length+1);
+
+            string parent = @"[\-\*\+][\+\!\[\]\(\)]+};";
+            
+            var tmp1 = @"var\st,r,a,f,\s" + var_without_obj + "={\"" + obj + "\":";
+
+            //выражение
+            var expression = GetFirstByRegex(tmp1 + parent + "", html);
+
+            expression = expression.Substring(expression.IndexOf(":") + 1, expression.Length - 4 - expression.IndexOf(":"));
+
+            List<string> list_expression = GetListByRegex(@"[\[\]\+\!]+", expression);
+
+            var p2i = parseIntList2Str(list_expression);
+            
+            string source = expression;
+
+            string tmp = GetFirstByRegex(@"\);\s+;" + var + ".*;a.value", html);
+
+            string v = tmp.Substring(tmp.IndexOf(var), tmp.Length - tmp.IndexOf(var) - "a.value".Length);
+            //WOlLQxg.xcWIr-=+((!+[]+!![]+[])+(!+[]+!![]+!![]+!![]+!![]+!![]));
+
+            List<string> list_exp = GetListByRegex(var + @"[\-\*\+]=[\+\!\[\]\(\)]+", html);
+
+            foreach (string exp in list_exp)
+            {
+                var znak = GetFirstByRegex(@"[\-\*\+]=", exp).Substring(0,1);
+                list_expression = GetListByRegex(@"[\[\]\+\!]+", exp);
+                var p2i_new = parseIntList2Str(list_expression);
+
+                if(znak == "-")
+                    p2i = Minus(p2i, p2i_new);
+                else if(znak == "+")
+                    p2i = Plus(p2i, p2i_new);
+                else if(znak == "*")
+                    p2i = Multiply(p2i, p2i_new);
+            }
+            p2i = Plus(p2i,t.ToString());
+            
+        }
+    }
+}
